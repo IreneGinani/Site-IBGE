800ff6ff3f28f8ba43490bc50b741ddc6a7b376f
ireneginani <ireneginani@gmail.com>
2018-04-20 00:02:09 -0300

    feitas funcionalidades condizentes a população

--- polls/urls.py
+++ polls/urls.py
@@ -6,6 +6,9 @@ app_name = 'polls'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^popNatal$', views.popNatal, name='popNatal'),
+    url(r'^popMossoro$', views.popNatal, name='popMossoro'),
+    url(r'^popNatalMossoro$', views.popNatal, name='popNatalMossoro')
 
 
 ]
--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 import json
+import logging
 
 def index(request):
 
@@ -16,11 +17,58 @@ def popNatal(request):
 
 	response = requests.get('https://servicodados.ibge.gov.br/api/v1/projecoes/populacao/2403251')
 
-	if response.status_code in (201, 200):
-		parsed = json.loads(response.text)
-		logging.info(json.dumps(parsed, indent=4, sort_keys=True))
-		return parsed
-	else:
-		logging.error('PUT ROLE IN USER ### ' + response.text)
 
-	return render(request, 'index.html')
+	if response.status_code in (201, 200):
+
+		parsed = json.loads(response.text)
+		data = str(parsed['projecao']['populacao'])
+		print(data)
+		arq = open('polls/static/data-files/dataNatal.json', 'w')	
+		arq.close()
+		return HttpResponse('População de Natal: '+data+' habitantes')
+
+	else:
+
+		logging.error('ERROR EM POPULACAO NATAL ### ' + response.text)
+
+def popMossoro(request):
+
+	response = requests.get('https://servicodados.ibge.gov.br/api/v1/projecoes/populacao/2404408')
+
+
+	if response.status_code in (201, 200):
+
+		parsed = json.loads(response.text)
+		data = str(parsed['projecao']['populacao'])
+		arq = open('polls/static/data-files/dataMossoro.json', 'w')
+		arq.write(data) 	
+		arq.close()
+		return HttpResponse('População de Mossoro: '+data+' habitantes')
+
+	else:
+
+		logging.error('ERROR EM POPULACAO NATAL ### ' + response.text)
+
+def popNatalMossoro(request):
+
+	responseN = requests.get('https://servicodados.ibge.gov.br/api/v1/projecoes/populacao/2403251')
+	responseM = requests.get('https://servicodados.ibge.gov.br/api/v1/projecoes/populacao/2404408')
+
+
+	if response.status_code in (201, 200):
+
+		parsedN = json.loads(responseN.text)
+		parsedM = json.loads(responseM.text)
+		dataN = parsedN['projecao']['populacao']
+		dataM = parsedM['projecao']['populacao']
+		totalS = dataM + dataN
+		total = str(totalS)
+		arq = open('polls/static/data-files/dataMossoroNatal.json', 'w')
+		arq.write(data) 	
+		arq.close()
+		return HttpResponse('População de Natal e Mossoro: '+total+' habitantes')
+
+	else:
+
+		logging.error('ERROR EM POPULACAO NATAL ### ' + response.text)
+
